9280bf6e534139b1a7e51d95664e61ce24d057f9
Nishanth kulal <76471121+nishanthkulal@users.noreply.github.com>
2025-06-15 11:22:37 -0700

    Update : character movement and animations along with level generator added

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,16 +1,42 @@
+using System;
 using UnityEngine;
+
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform[] levelParts;
+    [SerializeField] private Vector3 nextPartPosition;
+    [SerializeField] private float distanceToSpawn;
+    [SerializeField] private float distanceToDespawn;
+    [SerializeField] private Transform player;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        deletePlatform();
+        GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        while (Vector2.Distance(player.position, nextPartPosition) < distanceToSpawn)
+        {
+            Transform part = levelParts[UnityEngine.Random.Range(0, levelParts.Length)];
+            Vector2 newPosition = new Vector2(nextPartPosition.x - part.Find("Start").position.x, 0);
+            Transform newPart = Instantiate(part, newPosition, transform.rotation, transform);
+            nextPartPosition = newPart.Find("End").position;
+        }
+    }
+
+    private void deletePlatform()
+    {
+        if (transform.childCount > 0)
+        {
+            Transform partToDelete = transform.GetChild(0);
+            if (Vector2.Distance(player.position, partToDelete.position) > distanceToDespawn)
+            {
+                Destroy(partToDelete.gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,62 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D playerRb;
+    private bool _startPlayerRunning = false;
+    private bool _isGrounded;
+    private Animator _playeranimator;
+
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        _playeranimator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        AnimatorController();
+        PlayerMov();
+        PlayerJump();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayerJump()
     {
-        
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, jumpForce);
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundDistance));
+    }
+
+    private void PlayerMov()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _startPlayerRunning = true;
+        }
+
+        if (_startPlayerRunning)
+        {
+            playerRb.linearVelocity = new Vector2(moveSpeed, playerRb.linearVelocity.y);
+        }
+    }
+
+    private void AnimatorController()
+    {
+        _playeranimator.SetBool("IsGrounded", _isGrounded);
+        _playeranimator.SetFloat("xVelocity", playerRb.linearVelocity.x);
+        _playeranimator.SetFloat("yVelocity", playerRb.linearVelocity.y);
+
     }
 }
