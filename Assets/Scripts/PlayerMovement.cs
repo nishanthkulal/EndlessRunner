08ed2761ce19a54d3d3c97ea2f8a35ebd323fea1
Nishanth kulal <76471121+nishanthkulal@users.noreply.github.com>
2025-06-17 11:35:18 -0700

    Update :  sliding , stoping when wall detected , dubble jump  animations updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,31 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
     [SerializeField] private float groundDistance;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Vector2 wallCheckSize;
+    [SerializeField] private float slidingSpeed;
+    [SerializeField] private float slideTime;
+    [SerializeField] private float slideCoolDownTime;
+    [SerializeField] private float checkDistanceToCelling;
+    private float slideCoolDownCounter;
+    private float slideTimeCounter;
+    private bool _isSliding;
     private Rigidbody2D playerRb;
     private bool _startPlayerRunning = false;
     private bool _isGrounded;
     private Animator _playeranimator;
+    private bool _canDoubleJump;
+    private bool _isWallDetected;
+    private bool _isCellingDetected;
+
 
     private void Start()
     {
@@ -19,25 +34,56 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
+        slideTimeCounter -= Time.deltaTime;
+        slideCoolDownCounter -= Time.deltaTime;
         AnimatorController();
         PlayerMov();
         PlayerJump();
+        PlayerSliding();
+        CheckForSlide();
+        if (_isGrounded)
+        {
+            _canDoubleJump = true;
+        }
 
     }
+
+
 
     private void PlayerJump()
     {
-        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+        WallDetection();
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpButton();
+        }
+    }
+
+    private void WallDetection()
+    {
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+        _isCellingDetected = Physics2D.Raycast(transform.position, Vector2.up, checkDistanceToCelling, groundLayer);
+        _isWallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, groundLayer);
+    }
+
+    private void JumpButton()
+    {
+        if (_isSliding)
+            return;
+        if (_isGrounded)
         {
             playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, jumpForce);
         }
+        else if (_canDoubleJump)
+        {
+            _canDoubleJump = false;
+            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, doubleJumpForce);
+        }
+
     }
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundDistance));
-    }
+
+
 
     private void PlayerMov()
     {
@@ -46,17 +92,65 @@ public class PlayerMovement : MonoBehaviour
             _startPlayerRunning = true;
         }
 
-        if (_startPlayerRunning)
+        if (_startPlayerRunning && !_isWallDetected)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+        if (_isSliding)
+        {
+            playerRb.linearVelocity = new Vector2(slidingSpeed, playerRb.linearVelocity.y);
+        }
+        else
         {
             playerRb.linearVelocity = new Vector2(moveSpeed, playerRb.linearVelocity.y);
         }
     }
+    private void CheckForSlide()
+    {
+        Debug.Log(slideTimeCounter);
+        if (slideTimeCounter < 0 && !_isCellingDetected)
+        {
+            _isSliding = false;
+        }
+    }
+
+    private void PlayerSliding()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SlideButton();
+        }
+    }
+
+    private void SlideButton()
+    {
+        if (playerRb.linearVelocity.x != 0 && slideCoolDownCounter < 0 && _isGrounded)
+        {
+            _isSliding = true;
+            slideTimeCounter = slideTime;
+            slideCoolDownCounter = slideCoolDownTime;
+        }
+
+    }
 
     private void AnimatorController()
     {
-        _playeranimator.SetBool("IsGrounded", _isGrounded);
         _playeranimator.SetFloat("xVelocity", playerRb.linearVelocity.x);
         _playeranimator.SetFloat("yVelocity", playerRb.linearVelocity.y);
+        _playeranimator.SetBool("CanDoubbleJump", _canDoubleJump);
+        _playeranimator.SetBool("IsGrounded", _isGrounded);
+        _playeranimator.SetBool("IsSliding", _isSliding);
 
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + checkDistanceToCelling));
+        Gizmos.DrawCube(wallCheck.position, wallCheckSize);
     }
 }
